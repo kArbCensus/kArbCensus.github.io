515a6732bb9cd2ff0fc6d29bf7ea8bbd8cdd44b5
Juniper Pasternak <juniper.pasternak@gmail.com>
2024-11-07 10:11:43 -0500

    Improve auth0-functions documentation

--- _shared/auth0-functions.d.ts
+++ _shared/auth0-functions.d.ts
@@ -2,6 +2,11 @@
 type Auth0Client = import("@auth0/auth0-spa-js").Auth0Client;
 type Auth0ClientOptions = import("@auth0/auth0-spa-js").Auth0ClientOptions;
 
+/**
+ * Creates and returns a configured Auth0Client.
+ * @param {Auth0ClientOptions} options client options
+ * @returns {Promise<Auth0Client>}
+ */
 declare function createAuth0Client(
   options: Auth0ClientOptions
 ): Promise<Auth0Client>;
--- _shared/auth0-functions.js
+++ _shared/auth0-functions.js
@@ -1,6 +1,7 @@
 /**
- * Creates and returns a configured Auth0Client
- * @returns {Promise<Auth0Client>}
+ * Creates and returns a configured Auth0Client.
+ * @param {import("@auth0/auth0-spa-js").Auth0ClientOptions} options client options
+ * @returns {Promise<import("@auth0/auth0-spa-js").Auth0Client>}
  */
 function createAuth0Client(options) {
   return auth0.createAuth0Client(options);
