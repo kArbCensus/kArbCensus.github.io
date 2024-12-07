c4cc2c5011b2295d5c638be39859db2467870262
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-12-07 11:07:09 -0500

    Plan out ideas in admin script

--- admin/scripts/creation-tools-script.js
+++ admin/scripts/creation-tools-script.js
@@ -12,8 +12,17 @@ function createNewTreeSpecies() {
     console.log("Stub");
 }
 //TODO: Config to check/set warning for new plot
+function configPlotWarning(plot) {
+    return true;
+}
 //TODO: Config to check/set warning for new species
+function configTreeWarning(species) {
+    return true;
+}
 //TODO: event listener to turn off all modal warnings if a modal is hidden
 // Should probably make it so that all modals have the same footer spacing warning or not
+// Refresh page on submission?
+// Since the survey shows your added tree, it has fine user feedback.
+// But here I feel a page refresh would assure me I did something
 //////////// TYPES TO MAKE INFO FROM DB WORK ////////////
 //# sourceMappingURL=creation-tools-script.js.map
--- admin/scripts/creation-tools-script.ts
+++ admin/scripts/creation-tools-script.ts
@@ -16,19 +16,21 @@ function createNewTreeSpecies() {
 }
 
 //TODO: Config to check/set warning for new plot
-function configPlotWarning(plot: number) : boolean
-{
+function configPlotWarning(plot: number): boolean {
     return true;
 }
 
 //TODO: Config to check/set warning for new species
-function configTreeWarning(species: string) : boolean
-{
+function configTreeWarning(species: string): boolean {
     return true;
 }
 
 //TODO: event listener to turn off all modal warnings if a modal is hidden
 
 // Should probably make it so that all modals have the same footer spacing warning or not
+
+// Refresh page on submission?
+// Since the survey shows your added tree, it has fine user feedback.
+// But here I feel a page refresh would assure me I did something
 
 //////////// TYPES TO MAKE INFO FROM DB WORK ////////////
