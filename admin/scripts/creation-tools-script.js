a3d864736233777f556a528c619d2c829efd7213
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-12-06 23:10:14 -0500

    Plan out logic for modal warnings

--- admin/scripts/creation-tools-script.js
+++ admin/scripts/creation-tools-script.js
@@ -11,5 +11,9 @@ function createNewPlot() {
 function createNewTreeSpecies() {
     console.log("Stub");
 }
+//TODO: Config to check/set warning for new plot
+//TODO: Config to check/set warning for new species
+//TODO: event listener to turn off all modal warnings if a modal is hidden
+// Should probably make it so that all modals have the same footer spacing warning or not
 //////////// TYPES TO MAKE INFO FROM DB WORK ////////////
 //# sourceMappingURL=creation-tools-script.js.map
--- admin/scripts/creation-tools-script.ts
+++ admin/scripts/creation-tools-script.ts
@@ -15,4 +15,12 @@ function createNewTreeSpecies() {
     console.log("Stub");
 }
 
+//TODO: Config to check/set warning for new plot
+
+//TODO: Config to check/set warning for new species
+
+//TODO: event listener to turn off all modal warnings if a modal is hidden
+
+// Should probably make it so that all modals have the same footer spacing warning or not
+
 //////////// TYPES TO MAKE INFO FROM DB WORK ////////////
