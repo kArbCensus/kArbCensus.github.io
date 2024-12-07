edcefd7b54947ea5612993178bcc466a27417a17
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-12-06 20:26:39 -0500

    Remove shared button HTML component

--- admin/admin-jquery-script.js
+++ admin/admin-jquery-script.js
@@ -3,8 +3,5 @@ $(function () {
     $('.pop-up-top').load('../admin/modal-components/modal-top.html');
 })
 $(function () {
-    $('.pop-up-confirm').load('../admin/modal-components/modal-confirm.html');
-})
-$(function () {
     $('.pop-up-warning').load('../admin/modal-components/modal-warning.html');
 })
