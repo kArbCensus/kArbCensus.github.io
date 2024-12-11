1afdcc819f607316ec87449bff36ef998d2acedb
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-12-11 14:48:34 -0500

    Remove unneeded comments form admin script

--- admin/scripts/admin-extra-script.js
+++ admin/scripts/admin-extra-script.js
@@ -5,12 +5,3 @@ $(function () {
 $(function () {
     $('.pop-up-warning').load('../admin/modal-components/modal-warning.html');
 })
-
-
-/*
-Todos:
-Make this ^ method in shared 
-Make all of the visibile shifts on a selecting a plot in a div to simplify
-Make a refresh
-Make modal confirm popup for admin only
-*/
