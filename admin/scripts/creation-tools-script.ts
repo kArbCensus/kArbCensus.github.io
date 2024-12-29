5c52be0dd1bacc08e4b40d9aaa18127d19cf3dd6
Teddy Jacobson <theodorejacobson64@gmail.com>
2024-12-29 16:22:18 -0500

    Remove TODO message for an implemented feature

--- admin/scripts/creation-tools-script.ts
+++ admin/scripts/creation-tools-script.ts
@@ -245,9 +245,6 @@ async function genericBinarySearch<T extends any>(array: T[], lookFor: T, start:
 }
 
 
-//TODO: create an event listener that makes update buttons work when pulled up and disabled when clicked
-// (If this isn't done, you can just spam the enter button to have this happen a ton of times cause async)
-
 //////////// TYPES TO MAKE INFO FROM DB WORK ////////////
 
 interface NewUserInfo {
